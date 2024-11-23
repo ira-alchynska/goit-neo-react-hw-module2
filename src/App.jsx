b90906edb573c68f38ec50141cb4b1cf257f4849
Iryna Alchynska <iren0988@gmail.com>
2024-11-23 22:09:03 +0100

    add: description component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
+import Description from './components/Description/Description';
 import './App.css';
 
 const App = () => {
@@ -53,11 +54,7 @@ const App = () => {
 
     return (
         <div className="wrapper">
-            <h1>Sip Happens Café</h1>
-            <p>
-                Please leave your feedback about our service by selecting one of
-                the options below.
-            </p>
+            <Description />
             <Options
                 onLeaveFeedback={updateFeedback}
                 totalFeedback={totalFeedback}
